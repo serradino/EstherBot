aa38d37070420558dd4ac9f962aa15a45062e17a
Michele <serradino@users.noreply.github.com>
2016-05-07 15:12:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi, I’m Johrny I will help you not to miss your stop during your journey.
-You can rely on me on both daily and one-time routes on public transports.
-Let me know your habits, where you leave from and where you go')
+            return bot.say('Hi, I’m Johrny I will help you not to miss your stop during your journey. You can rely on me on both daily and one-time routes on public transports. Let me know your habits, where you leave from and where you go')
                 .then(() => 'speak');
         }
     },
